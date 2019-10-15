ea526351506332105114fd08b0a4b7c5aa9b360d
Kyle Hastings <kylejhastings@gmail.com>
2019-10-15 13:47:37 -0400

    Added a new Icon. App now switches activities and puts background color to selected color from first. Activity titles now refelct what activity they are

--- app/src/main/java/edu/temple/coloractivityswitch/CanvasActivity.java
+++ app/src/main/java/edu/temple/coloractivityswitch/CanvasActivity.java
@@ -1,14 +1,28 @@
 package edu.temple.coloractivityswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class CanvasActivity extends AppCompatActivity {
+
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+        getSupportActionBar().setTitle(R.string.canvas_title);
+
+        layout = findViewById(R.id.CanvasBackground);
+
+        Intent intent = getIntent();
+
+        String backgroundColor = (String) intent.getSerializableExtra("BACKGROUND");
+
+        layout.setBackgroundColor(Color.parseColor(backgroundColor));
     }
 }
--- app/src/main/java/edu/temple/coloractivityswitch/PaletteActivity.java
+++ app/src/main/java/edu/temple/coloractivityswitch/PaletteActivity.java
@@ -2,13 +2,59 @@ package edu.temple.coloractivityswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class PaletteActivity extends AppCompatActivity {
+
+    String[] colors;
+    int readyCheck = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle(R.string.palette_title);
+        Resources res = getResources();
+
+
+
+        final Spinner spinner = findViewById(R.id.spinner);
+
+        colors = res.getStringArray(R.array.colors);
+
+        ColorAdapter adapter = new ColorAdapter(PaletteActivity.this, colors);
+
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener select = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(readyCheck++ > 0){
+                    Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                    intent.putExtra("BACKGROUND", colors[position]);
+                    startActivity(intent);
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+        };
+        spinner.setOnItemSelectedListener(select);
+
+
     }
 }
